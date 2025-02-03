29f840039fc8e0c9d4644c77517dd8587c416cba
TheJ0lly <eusicumine7@gmail.com>
2025-02-03 16:09:47 +0200

    Added message basic handling.

---
+++ Network/Message.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using System.Windows.Controls;
+
+namespace ChatRoom.Network
+{
+    public struct Message
+    {
+        public string? User { get; set; }
+        public string? Time { get; set; }
+        public string? Text { get; set; }
+    }
+
+    public class MessageView : ListBoxItem
+    {
+        public MessageView(Message msg)
+        {
+            Content = $"{msg.Time} - {msg.User}: {msg.Text}";
+        }
+    }
+
+    public static class MessageManager
+    {
+        public static string ToJson(Message message)
+        {
+            return JsonSerializer.Serialize(message);
+        }
+
+        public static Message? FromJson(string json)
+        {
+            return JsonSerializer.Deserialize<Message>(json);
+        }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ChatRoom
 {
@@ -23,7 +14,6 @@ namespace ChatRoom
 
         private void HostServerButton_Click(object sender, RoutedEventArgs e)
         {
-            
         }
 
         private void JoinServerButton_Click(object sender, RoutedEventArgs e)
