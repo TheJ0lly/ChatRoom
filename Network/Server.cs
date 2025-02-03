22c878b365dfc5b7adcb17c18c9c6bd370e2e7a0
TheJ0lly <eusicumine7@gmail.com>
2025-02-03 18:16:40 +0200

    Added basic networking structure.

--- Network/Server.cs
+++ Network/Server.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
+
 
 namespace ChatRoom.Network
 {
@@ -14,13 +13,18 @@ namespace ChatRoom.Network
         private TcpListener _listener;
         private List<TcpClient> _clients;
         private string _name;
-        private Task? _receiver;
+        private object mutex = new object();
+        public string IP { get; private set; } = "";
+        public string Port { get; private set; } = "";
+
+        public CancellationTokenSource Token { get; }
         
         public Server(string serverName)
         {
             _listener = new TcpListener(new IPEndPoint(IPAddress.Any, 0));
             _clients = new List<TcpClient>();
             _name = serverName;
+            Token = new CancellationTokenSource();
         }
 
         /// <summary>
@@ -46,18 +50,122 @@ namespace ChatRoom.Network
 
             return true;
         }
+        public int ActiveConnections()
+        {
+            return _clients.Count;
+        }
 
-        public bool WriteMessage(string message)
+        public void Start(ListBox chatbox)
+        {
+            _listener.Start();
+
+            // We get the data of the server so that we know where to connect to
+            IPEndPoint localEndPoint = (IPEndPoint)_listener.LocalEndpoint;
+            Port = localEndPoint.Port.ToString();
+
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                // Make sure the interface is operational and not a loopback or virtual interface
+                if (ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    // Get the IP properties for this network interface
+                    IPInterfaceProperties ipProperties = ni.GetIPProperties();
+
+                    foreach (UnicastIPAddressInformation ip in ipProperties.UnicastAddresses)
+                    {
+                        // Look for IPv4 addresses (ignoring loopback address 127.0.0.1 and other conditions)
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork && !ip.Address.ToString().StartsWith("127"))
+                        {
+                            IP = ip.Address.ToString();
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // We start a Task running in the background that will handle incoming requests.
+            Task.Factory.StartNew(() => 
+            {
+                while (true)
+                {
+                    if (Token.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                    TcpClient client;
+
+                    try
+                    {
+                        client = _listener.AcceptTcpClient();
+                        _clients.Add(client);
+                    }
+                    catch
+                    {
+                        // In case of any errors we simply break and let it crash
+                        break;
+                    }
+
+                    // We start a Task for each client to listen to incoming messages
+                    Task.Factory.StartNew(() =>
+                    {
+                        var buff = new byte[4096];
+                        while (true)
+                        {
+                            if (Token.IsCancellationRequested)
+                            {
+                                break;
+                            }
+
+                            try
+                            {
+                                // In some cases the client may close the connection before reading EOF
+                                // thus it will throw an IOException, but we don't really care because it will simply crash the task
+                                var read = client.GetStream().Read(buff, 0, buff.Length);
+                                // If we read 0, it means the connection closed.
+                                if (read == 0)
+                                    break;
+
+                                var json = Encoding.UTF8.GetString(buff, 0, read);
+
+                                // We write to the file
+                                lock (mutex)
+                                {
+                                    WriteMessage(json);
+                                }
+
+                                // We add the visual
+                                chatbox.Dispatcher.Invoke(() =>
+                                {
+                                    chatbox.Items.Add(new MessageView(MessageManager.FromJson(json)));
+                                });
+                            }
+                            catch
+                            { 
+                                // In case of any error, we simply let the task crash, and remove the client from the list.
+                                _clients.Remove(client);
+                            }
+                        }
+                    }, Token.Token);
+                }
+            }, Token.Token);
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            Token.Cancel();
+        }
+
+        private bool WriteMessage(string message)
         {
             using (StreamWriter sw = new StreamWriter($"Servers\\{_name}\\chat.txt", append: true))
             {
                 sw.WriteLine(message);
             }
-
             return true;
         }
 
-        public List<MessageView> ReadMessage()
+        public List<MessageView> ReadChat()
         {
             var messages = new List<MessageView>();
             using (StreamReader sr = new StreamReader($"Servers\\{_name}\\chat.txt"))
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using ChatRoom.Network;
 
 namespace ChatRoom
@@ -17,12 +18,31 @@ namespace ChatRoom
 
         private void HostServerButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (e.OriginalSource is MenuItem button && button.Header.ToString() == "Existing")
+            {
+                MessageBox.Show($"Current active connections {_activeServer?.ActiveConnections()}");
+                return;
+            }
+
+            _activeServer = new Server("Test");
+            if (!_activeServer.Create())
+            {
+                MessageBox.Show("Error", "Cannot create the new server", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+
+            _activeServer.Start(ChatBox);
+            IpLabel.Content = _activeServer.IP;
+            PortLabel.Content = _activeServer.Port;
         }
 
         private void JoinServerButton_Click(object sender, RoutedEventArgs e)
         {
-
+            _activeServer?.Stop();
+            ChatBox.Items.Clear();
+            IpLabel.Content = "None";
+            PortLabel.Content = "None";
         }
     }
 }
